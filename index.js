a485d3aa77dbca1aed7af5915730911832ebb3d3
piyush9pp <152002428+piyush9pp@users.noreply.github.com>
2025-01-18 20:40:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,22 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+// app.use(
+// 	cors({
+// 		origin:"http://localhost:3000",
+// 		credentials:true,
+// 	})
+	
+// )
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://studynotionbackend-1hfi.onrender.com"
+    ],
+    credentials: true,
+  })
+);
 
 app.use(
 	fileUpload({
